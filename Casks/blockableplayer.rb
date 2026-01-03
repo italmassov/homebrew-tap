e00878a5ba321132a507f52aacc170dcf1cd686d
Kuanysh Italmassov <italmas2010@gmail.com>
2026-01-02 22:10:41 -0500

    Update blockableplayer.rb

--- Casks/blockableplayer.rb
+++ Casks/blockableplayer.rb
@@ -1,11 +1,11 @@
 cask "blockableplayer" do
   version "0.1.0"
-  sha256 "__SHA256__"
+  sha256 "90457ce784a6101a97e2461a8fbbbe498760ae6f3da30a928d5c7dd8d498a6fa"
 
-  url "__URL__"
+  url "https://github.com/italmassov/blockable_player/releases/download/v0.1.0/BlockablePlayer.zip"
   name "BlockablePlayer"
   desc "Blockable video player for macOS"
-  homepage "__HOMEPAGE__"
+  homepage "https://github.com/italmassov/blockable_player"
 
   app "BlockablePlayer.app"
 end
